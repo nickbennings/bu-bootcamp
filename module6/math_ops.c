1849e576de04482ee7468742ca52c8f86803ed1b
Nicholas Bennings <ncbennings@gmail.com>
2026-08-19 12:08:42 -0400

    Add Module 6 C programs: math_ops and swap

---
+++ module6/math_ops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_math(int a, int b)
+{
+    printf("Sum: %d\n", a + b);
+    printf("Product: %d\n", a * b);
+}
+
+int main()
+{
+    int firstNumber;
+    int secondNumber;
+
+    printf("Enter first number: ");
+    scanf("%d", &firstNumber);
+
+    printf("Enter second number: ");
+    scanf("%d", &secondNumber);
+
+    print_math(firstNumber, secondNumber);
+
+    return 0;
+}
+
---
+++ module6/swap.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void broken_swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    printf("Before broken_swap: x = %d, y = %d\n", x, y);
+
+    broken_swap(x, y);
+
+    printf("After broken_swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
